74b98cfb9bd2f4ffaf8a0f2a0d5f4a55f94db495
tim-morozov <tim.morozov@gmail.com>
2020-04-20 09:55:07 -0500

    created a method to randomly generate a new customer based on alist of names in the Customer class

--- LemonadeStand/LemonadeStand_3DayStarter/Customer.cs
+++ LemonadeStand/LemonadeStand_3DayStarter/Customer.cs
@@ -10,5 +10,24 @@ namespace LemonadeStand_3DayStarter
     {
         public string customerName;
         private List<string> names;
+
+        public Customer()
+        {
+            names = new List<string>() { "Bill", "Jane", "Todd", "Jack", "Jenny", "Suzy" };
+            customerName = CustomerGenerator();
+        }
+        public int RandomGenerator(int min, int max)
+        {
+            Random random = new Random();
+            int newRandom = random.Next(min, max);
+            return newRandom;
+        }
+
+        private string CustomerGenerator()
+        {
+            int i = RandomGenerator(0, (names.Count() - 1));
+            string customer = names[i];
+            return customer;
+        }
     }
 }
