e522fed7f015c92dfb24da2386ed0c8597467b7d
tim-morozov <tim.morozov@gmail.com>
2020-04-21 10:58:23 -0500

    created a CustomerGenerator method in the Day class to generate different amounts of customer dependent on weather and temperature

--- LemonadeStand/LemonadeStand_3DayStarter/Day.cs
+++ LemonadeStand/LemonadeStand_3DayStarter/Day.cs
@@ -17,5 +17,33 @@ namespace LemonadeStand_3DayStarter
             weather = new Weather();
             customers = new List<Customer>();
         }
+
+        private void CustomerGenerator()
+        {
+            if (weather.condition == "Sunny" || weather.temperature >= 80)
+            {
+                for (int i = 0; i <= 30; i++)
+                {
+                    Customer customer = new Customer();
+                    customers.Add(customer);
+                }
+            }
+            else if (weather.condition == "Rainy" || weather.temperature <= 60)
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    Customer customer = new Customer();
+                    customers.Add(customer);
+                }
+            }
+            else
+            {
+                for(int i = 0; i <= 20; i++)
+                {
+                    Customer customer = new Customer();
+                    customers.Add(customer);
+                }
+            }    
+        }
     }
 }
